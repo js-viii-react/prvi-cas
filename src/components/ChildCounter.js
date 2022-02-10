d2e0b4c1b6c57233683bc1978a66c48beab4d048
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2022-02-10 22:27:18 +0100

    added props, handling events through props

--- src/components/ChildCounter.js
+++ src/components/ChildCounter.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ChildCounter extends Component {
-    state = { 
-      
-     } 
-    render() { 
-        return (
-            <div>#{this.props.value}
-            
-            </div>
-        );
-    }
+  //   state = {
+  //     value: this.props.value,
+  //   };
+
+  //   handleIncrement = () => {
+  //     this.setState({ value: this.state.value + 1 });
+  //   };
+
+  render() {
+    return (
+      <div style={{ margin: 15 }}>
+        #{this.props.counter.value}
+        <button onClick={() => this.props.onIncrement(this.props.counter.id)}>
+          +
+        </button>
+        <button
+          style={{ marginLeft: 5 }}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
 }
- 
+
 export default ChildCounter;
