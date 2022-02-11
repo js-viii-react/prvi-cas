c0cc85f0c8ab0c22064eb89e355bd5481069052e
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2022-02-11 22:09:16 +0100

    added Navbar as functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from  "./components/Counter"
-import Counters from  "./components/Counters"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import Counters from "./components/Counters";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className='my-app'>
-      {/* Iza viticastih zagrada 2+2 = { 2+2 } */}
-      {/* <Counter /> */}
-      <Counters />
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 0, value: 0 },
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="my-app">
+        {/* Iza viticastih zagrada 2+2 = { 2+2 } */}
+        {/* <Counter /> */}
+        <Navbar totalCounters={this.state.counters.length} />
+        <Counters />
+      </div>
+    );
+  }
 }
 
 export default App;
